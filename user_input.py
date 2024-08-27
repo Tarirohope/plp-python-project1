6953c83b4f9d197e7a5eadf5b2c8feba98e30ae5
Priscila Tariro Kapuyanyika <Priscilakapuyanyika974@gmail.com>
2024-08-27 12:06:55 +0200

    I added a file user_input.py

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("What is your current location? ")
+print(f"Hello {name} you are {age} years old and currently you are in {location}")
